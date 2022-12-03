cf5d348d97552d7ae10d5b6261ee572311ebf0ec
Ari Mirsky <arimirsky@gmail.com>
2022-12-03 02:11:44 -0500

    Day 3

--- day3/p1.py
+++ day3/p1.py
@@ -5,7 +5,15 @@ lines = input.split("\n")
 
 ans = 0
 for line in lines:
-    pass
+    mid = int(len(line)/2)
+    f = set(line[0:mid])
+    s = set(line[mid:])
+    a = f.intersection(s)
+    for i in a:
+        if i.islower():
+            ans += ord(i) - ord('a') + 1
+        else:
+            ans += ord(i) - ord('A') + 27
 
 print(ans)
-#submit(ans, part="a", day=3, year=2022)
+submit(ans, part="a", day=3, year=2022)
--- day3/p2.py
+++ day3/p2.py
@@ -4,8 +4,23 @@ input = get_data(day=3, year=2022)
 lines = input.split("\n")
 
 ans = 0
+elf = 0
+rucks = ["", "", ""]
 for line in lines:
-    pass
+    rucks[elf] = line
+    if elf == 2:
+        f = set(rucks[0])
+        s = set(rucks[1])
+        t = set(rucks[2])
+        a = f.intersection(s).intersection(t)
+        for i in a:
+            if i.islower():
+                ans += ord(i) - ord('a') + 1
+            else:
+                ans += ord(i) - ord('A') + 27
+    elf += 1
+    if elf == 3:
+        elf = 0
 
 print(ans)
-#submit(ans, part="b", day=3, year=2022)
+submit(ans, part="b", day=3, year=2022)
