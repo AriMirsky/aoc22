3f0bf353fb71ab0837f2ae29205aedd8bbc18b43
Ari Mirsky <arimirsky@gmail.com>
2022-12-11 15:11:03 -0500

    Garbage day 9 code

--- day9/p2.py
+++ day9/p2.py
@@ -5,9 +5,11 @@ lines = input.split("\n")
 
 
 class Rope():
+    tail = [0, 0]
+    head = [0, 0]
+
     def __init__(self):
-        self.tail = [0, 0]
-        self.head = [0, 0]
+        pass
 
     def move(self, direction):
         self.head[0] += direction[0]
@@ -27,7 +29,7 @@ class Rope():
             self.tail[0] = self.head[0]
 
     def tailPos(self):
-        return [self.tail[0], self.tail[1]]
+        return self.tail
 
 
 def dirConversion(dir):
@@ -42,29 +44,14 @@ def dirConversion(dir):
 
 
 ans = 0
-ropes = [Rope() for _ in range(10)]
-prevPrevPoss = [[0, 0] for _ in range(10)]
-prevPoss = [[0, 0] for _ in range(10)]
+rope = Rope()
 reached = set()
 reached.add((0, 0))
 for line in lines:
     cmds = line.split(" ")
     for _ in range(int(cmds[1])):
-        ropes[0].move(dirConversion(cmds[0]))
-        print(cmds)
-        print(dirConversion(cmds[0]))
-        for i in range(1, 10):
-            ropes[i].move([-prevPrevPoss[i - 1][0] + ropes[i - 1].tailPos()
-                          [0], -prevPrevPoss[i - 1][1] + ropes[i - 1].tailPos()[1]])
-            print([-prevPrevPoss[i - 1][0] + ropes[i - 1].tailPos()
-                   [0], -prevPrevPoss[i - 1][1] + ropes[i - 1].tailPos()[1]])
-        for i in range(10):
-            prevPrevPoss[i] = prevPoss[i]
-            prevPoss[i] = ropes[i].tailPos()
-            print(ropes[i].tail, ropes[i].head,
-                  "     ", prevPrevPoss[i], prevPoss[i])
-        print()
-        reached.add(tuple(ropes[9].tailPos()))
+        rope.move(dirConversion(cmds[0]))
+        reached.add(tuple(rope.tailPos()))
 ans = len(reached)
 print(ans)
 #submit(ans, part="b", day=9, year=2022)
